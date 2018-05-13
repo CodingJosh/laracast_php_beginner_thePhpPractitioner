a0468d5e6c3ea023f2b6a3a446796b6223f288d6
CodingJosh <39236942+CodingJosh@users.noreply.github.com>
2018-05-13 15:49:29 +0200

    Step 4: PHP and HTML

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<?php
-
-$greeting = 'Joshua';
-
-echo 'Hello,' . $greeting;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Document</title>
+    <style>
+        header{
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>
+            <?=
+                echo 'Hello, ' . htmlspecialchars($_GET['name']);
+            ?>
+        </h1>
+    </header>
+</body>
+</html>
